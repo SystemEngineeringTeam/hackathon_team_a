baa93952daf365d7bec954b3e048ce343b449067
momongya <mo5e2ka8@gmail.com>
2021-03-15 16:35:37 +0900

    メニュー画面(仮)(文字やイラスト以外)設置

---
+++ hackathon_project/Assets/Scripts/GoToMenu.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GoToMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    //ボタンを押した時の処理
+    public void PushButton()
+    {
+        SceneManager.LoadScene("Scenes/Menu");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
