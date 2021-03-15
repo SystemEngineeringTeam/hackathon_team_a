7a4334ebd45d56b00e3a66eab2e2c9305a109373
momongya <mo5e2ka8@gmail.com>
2021-03-15 15:35:39 +0900

    タイトル(仮)と画面遷移用のボタン設置

---
+++ hackathon_project/Assets/Scripts/WaitingPlaceFromTitle.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class WaitingPlaceFromTitle : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    //ボタンを押した時の処理
+    public void PushButton()
+    {
+        SceneManager.LoadScene("Scenes/WaitingPlace");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
