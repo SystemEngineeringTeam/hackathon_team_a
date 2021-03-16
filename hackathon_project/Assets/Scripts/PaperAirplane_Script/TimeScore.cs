f2dd73b490fa76ddbfbacb80b6dc822d22f23aa6
wdtkr <wdtkru@icloud.com>
2021-03-16 14:31:25 +0900

    ボタンの透明化、クリア画面へのスコア渡し

---
+++ hackathon_project/Assets/Scripts/PaperAirplane_Script/TimeScore.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class TimeScore : MonoBehaviour
+{
+    float time = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        time = TimeCounter.Count();
+        GetComponent<Text>().text = "Time　" + time.ToString("F2") + "s";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    // public void OnText(){
+        
+    // }
+}
