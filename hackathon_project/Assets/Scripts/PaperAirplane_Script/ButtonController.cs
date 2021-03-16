aa59dc146043906d4c99dea01dfac48898fa6dec
wdtkr <wdtkru@icloud.com>
2021-03-16 18:24:51 +0900

    ゲーム内からメニュー画面へ戻るボタンを追加

--- hackathon_project/Assets/Scripts/PaperAirplane_Script/ButtonController.cs
+++ hackathon_project/Assets/Scripts/PaperAirplane_Script/ButtonController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ButtonController : MonoBehaviour
 {
@@ -21,5 +22,8 @@ public class ButtonController : MonoBehaviour
     }
     public void buttonfalse(){
         text.SetActive(false);
+    }
+    public void goHome(){
+        SceneManager.LoadScene("Menu");
     }
 }
