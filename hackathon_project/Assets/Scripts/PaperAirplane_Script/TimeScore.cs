326ceec6a9ee2bd182d6e7c878aecb784d6247b0
wdtkr <wdtkru@icloud.com>
2021-03-16 15:05:51 +0900

    ハイスコアの実装

--- hackathon_project/Assets/Scripts/PaperAirplane_Script/TimeScore.cs
+++ hackathon_project/Assets/Scripts/PaperAirplane_Script/TimeScore.cs
@@ -5,11 +5,19 @@ using UnityEngine.UI;
 public class TimeScore : MonoBehaviour
 {
     float time = 0;
+    float highScore = 10000f; //スコア用
+    private string key = "HIGHSCORE"; //ハイスコア保存先キー
     // Start is called before the first frame update
     void Start()
     {
         time = TimeCounter.Count();
-        GetComponent<Text>().text = "Time　" + time.ToString("F2") + "s";
+        highScore = PlayerPrefs.GetFloat(key,10000f);
+        if(time < highScore){
+            highScore = time; //ハイスコア更新
+            PlayerPrefs.SetFloat(key,highScore);
+        }
+        //保存しておいたハイスコアをキーで呼び出し取得し、保存されていなければ0になる
+        GetComponent<Text>().text ="Best " + highScore.ToString("F2") + "s\n" + "Time　" + time.ToString("F2") + "s";
     }
 
     // Update is called once per frame
