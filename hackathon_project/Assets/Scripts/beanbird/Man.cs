884effa387ba787a9d08bacb79af4e776f2a2fa9
x20064Naruse <yoy11029mc@gmail.com>
2021-03-17 10:22:07 +0900

    プレイヤーの操作方法

--- hackathon_project/Assets/Scripts/beanbird/Man.cs
+++ hackathon_project/Assets/Scripts/beanbird/Man.cs
@@ -6,6 +6,34 @@ public class PixelObjectBase : MonoBehaviour
 {
     private Vector3 cashPosition;
 
+    public float speed;
+    private Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = GetComponent();
+    }
+
+    void FixedUpdate()
+    {
+        float horizontalKey = Input.GetAxis("Horizontal");
+        //右入力で左向きに動く
+        if(horizontalKey > 0)
+        {
+            rb.velocity = new Vector2(speed, rb.velocity.y);
+        }
+        //左入力で左向きに動く
+        else if(horizontalKey < 0)
+        {
+            rb.velocity = new Vector2(-speed, rb.velocity.y);
+        }
+        //ボタンを話すと止まる
+        else
+        {
+            rb.velocity = Vector2.zero;
+        }
+    }
+
     void LateUpdate()
     {
         cashPosition = transform.localPosition;
