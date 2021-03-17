750d9b980d2d04aac9820025f7650ebc486b6f15
wdtkr <wdtkru@icloud.com>
2021-03-17 16:54:22 +0900

    新ゲームの説明画面以外の完成

---
+++ hackathon_project/Assets/Scripts/Snake/HighOnly.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class HighOnly : MonoBehaviour
+{
+    int score = 0;
+    int highScore = 0; //スコア用
+    private string key = "HIGHSCORE"; //ハイスコア保存先キー
+    // Start is called before the first frame update
+    void Start()
+    {
+        score = ZoomScale.GetScore();
+        highScore = PlayerPrefs.GetInt(key,0);
+        if(score > highScore){
+            highScore = score; //ハイスコア更新
+            PlayerPrefs.SetInt(key,highScore);
+        }
+        //保存しておいたハイスコアをキーで呼び出し取得し、保存されていなければ0になる
+        GetComponent<Text>().text ="ハイスコア\n" + highScore;
+    }
+
+}
