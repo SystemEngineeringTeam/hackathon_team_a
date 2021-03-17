ee3f88ac6a7de794690e5ae5c75875bbb655dab2
x20064Naruse <yoy11029mc@gmail.com>
2021-03-17 12:11:50 +0900

    プレイヤーのアニメーションの追加 ちゃんと歩けるようにした

---
+++ hackathon_project/Assets/Scripts/beanbird/Player_man.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_man : MonoBehaviour
+{
+    private Animator anim = null;
+    private Rigidbody2D rb = null;
+
+    public float speed;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+
+    void Update()
+    {
+        float horizontalKey = Input.GetAxis("Horizontal"); 
+        float xSpeed = 0.0f;
+
+        if (horizontalKey > 0) 
+        {
+            transform.localScale = new Vector3(1, 1, 1);
+            anim.SetBool("run", true);
+            xSpeed = speed;
+        }
+        else if (horizontalKey < 0) 
+        {
+            transform.localScale = new Vector3(-1, 1, 1);
+            anim.SetBool("run", true);
+            xSpeed = -speed;
+        }
+        else
+        {
+            anim.SetBool("run", false);
+            xSpeed = 0.0f;
+        }
+        rb.velocity = new Vector2(xSpeed, rb.velocity.y);
+    }
+}
