01d55c7b08cd7b00211d1ef50ce195c952b509c3
wdtkr <wdtkru@icloud.com>
2021-03-18 15:00:10 +0900

    色違いボルケニオンゲット！ 簡単です！ まず初めに ボックス1の一番左上にポケモンがいないことを確認する。 いるなら別の場所に移動してください。 データを消して最初から始める。(データの消し方：十字キーの上ボタン+Bボタン+Xボタン) 最後にデータはちゃんと戻るので安心してください！ 注意！前のデータが女主人公でも必ず男主人公にすること！女主人公で試したところ全て失敗しています。適当でいいのでレポートを書くところまで進める。「レポートを書き残した！」の表示が出た瞬間に電源を切ります。タイミングが難しいので私はソフトを抜く方法でやってます。成功率は個人的にこっちのほうが高い気がします。注意！必ず一番最初に書くレポートで↑をやってください。これをやる前にレポートを書いていると失敗します。 ソフトを再開すると「続きから」が消す前のデータに戻っています。 ボックスを確認するとレベル1の色違いボルケニオンがいます。 結果 ボルケニオンゲット！

--- hackathon_project/Assets/Scripts/ScreenTransition/GoToWaitingPlace.cs
+++ hackathon_project/Assets/Scripts/ScreenTransition/GoToWaitingPlace.cs
@@ -5,15 +5,17 @@ using UnityEngine.SceneManagement;
 
 public class GoToWaitingPlace : MonoBehaviour
 {
+    GameObject sound;
     // Start is called before the first frame update
     void Start()
     {
-        
+        sound = GameObject.Find("EventSystem");
     }
 
     //ボタンを押した時の処理
     public void PushButton()
     {
+        sound.SetActive(false);
         SceneManager.LoadScene("Scenes/WaitingPlace");
     }
 
